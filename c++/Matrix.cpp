ec617f667191a42e5ae6bad63faa3f3683ca30c2
cuc <cucumberhenge@proton.me>
2025-02-27 11:43:30 +0200

    new version of Matrix

--- c++/Matrix.cpp
+++ c++/Matrix.cpp
@@ -1,114 +1,297 @@
-#include <iostream>
-#include <string>
-#include <vector>
 
+#include <iostream>
+#include <concepts>
+#include <string>
 using namespace std;
 
+template<class type, int row, int column> 
 class Matrix
 {
-    friend ostream& operator<< (ostream&, Matrix);
-    friend Matrix& operator++(Matrix&);
-    friend Matrix& operator--(Matrix&);
+    template<class t, int m, int n> 
+    friend ostream& operator<<(ostream&, Matrix<t, m, n>);
 
     public:
 
     Matrix();
-    Matrix(int, int, int);
-    Matrix(vector<vector<int>>&);
-    Matrix(Matrix&);
+    Matrix(type);
+    // Matrix(type[row][column]);
+    Matrix(const Matrix&);
     ~Matrix();
 
-    int det();
-    Matrix minor(int, int);
-    Matrix trans();
-    Matrix adj();
-    Matrix invert(int, int);
-    int get_n();
+    Matrix& operator=(Matrix);
 
-    Matrix operator+ (Matrix&);
-    Matrix operator- (Matrix&);
-    Matrix operator+ (int);
-    Matrix operator- (int);
+    Matrix& operator+=(const Matrix&);
+    Matrix& operator+=(int);
+    Matrix operator+(Matrix);
+    Matrix operator+(int);
 
-    Matrix& operator+= (Matrix&);
-    Matrix& operator-= (Matrix&);
-    Matrix& operator+= (int);
-    Matrix& operator-= (int);
+    Matrix& operator-=(const Matrix&);
+    Matrix& operator-=(int);
+    Matrix operator-(Matrix);
+    Matrix operator-(int);
 
-    Matrix operator* (Matrix&);
-    Matrix operator* (int);
-    Matrix operator% (int);
+    Matrix& operator*=(const Matrix&); // завтра днем потестить надо
+    Matrix& operator*=(int);
+    Matrix operator*(Matrix);
+    Matrix operator*(int);
 
-    vector<int>& operator[] (int);
+    Matrix& operator/=(int);
+    Matrix operator/(int);
+
+    Matrix& operator++();
+    Matrix operator++(int);
+
+    Matrix& operator--();
+    Matrix operator--(int);
+
+    Matrix<type, row-1, column-1> minor(int, int) const;
+    int det() const; // дописать надо для матриц порядка выше 2
+    Matrix transposition() const;
+    void set(type, int, int);
+    pair<type, type> get_size();
+
+    type* operator[](int);
+    type* operator[](int) const;
+    type& at(int, int);
+
+    class UniversalError;
 
     protected:
-
-    int m, n;
-    vector<vector<int>> matr;
+    const int m = row;
+    const int n = column;
+    type** matrix;
 };
 
-Matrix::Matrix():
-m(0), n(0)
+template<class type, int row, int column>
+Matrix<type, row, column>::Matrix():
+Matrix(0)
 {}
 
-Matrix::Matrix(int m, int n, int c = 0):
-m(m), n(n)
+template<class type, int row, int column>
+Matrix<type, row, column>::Matrix(type value):
+matrix(new type*[row])
 {
-    for (int i = 0; i<m; i++)
+    for (int i = 0; i<row; ++i)
     {
-        matr.push_back(vector<int>());
-        for (int j = 0; j<n; j++)
+        matrix[i] = new type[column];
+        for (int j = 0; j<column; ++j)
+        { matrix[i][j] = (i == j) ? value : 0; }
+    }
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column>::Matrix(const Matrix& another):
+matrix(new type*[row])
+{
+    for (int i = 0; i<row; ++i)
+    {
+        matrix[i] = new type[column];
+        for (int j = 0; j<column; ++j)
+        { matrix[i][j] = another[i][j]; }
+    }
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column>::~Matrix()
+{
+    for (int i = m-1; i >= 0; --i)
+    { delete[] matrix[i]; }
+
+    delete[] matrix;
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column>& Matrix<type, row, column>::operator=(Matrix another)
+{
+    for (int i = 0; i<row; ++i)
+    {
+        matrix[i] = new type[column];
+        for (int j = 0; j<column; ++j)
+        { matrix[i][j] = another[i][j]; }
+    }
+
+    return *this;
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column>& Matrix<type, row, column>::operator+=(const Matrix& another)
+{
+    for (int i = 0; i<row; ++i)
+    {
+        for (int j = 0; j<column; ++j)
+        { matrix[i][j] += another[i][j]; }
+    }
+
+    return *this;
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column>& Matrix<type, row, column>::operator+=(int num)
+{ return *this += Matrix<type, row, column>(num); }
+
+template<class type, int row, int column>
+Matrix<type, row, column> Matrix<type, row, column>::operator+(Matrix another)
+{
+    Matrix copy {*this};
+    copy += another;
+    return copy;
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column> Matrix<type, row, column>::operator+(int num)
+{ return *this + Matrix<type, row, column>(num); }
+
+template<class type, int row, int column>
+Matrix<type, row, column>& Matrix<type, row, column>::operator-=(const Matrix& another)
+{ return *this += -1 * another; }
+
+template<class type, int row, int column>
+Matrix<type, row, column>& Matrix<type, row, column>::operator-=(int num)
+{ return *this -= Matrix<type, row, column>(num); }
+
+template<class type, int row, int column>
+Matrix<type, row, column> Matrix<type, row, column>::operator-(Matrix another)
+{
+    Matrix copy {*this};
+    copy -= another;
+    return copy;
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column> Matrix<type, row, column>::operator-(int num)
+{ return *this - Matrix<type, row, column>(num); }
+
+template<class type, int row, int column>
+Matrix<type, row, column>& Matrix<type, row, column>::operator*=(const Matrix& another)
+{
+    Matrix<type, row, column> tmp;
+    for (int i = 0; i<row; ++i)
+    {
+        for (int j = 0; j<column; ++j)
         {
-            if (i == j) matr[matr.size()-1].push_back(c);
-            else matr[matr.size()-1].push_back(0);
+            for (int k = 0; k<row; ++k)
+            { tmp[i][j] += matrix[i][k]*another[k][j]; }
         }
-    } 
+    }
+
+    return *this = tmp;   
 }
 
-Matrix::Matrix(vector<vector<int>>& key):
-matr(key) 
+template<class type, int row, int column>
+Matrix<type, row, column>& Matrix<type, row, column>::operator*=(int num)
+{ 
+    for (int i = 0; i<row; ++i)
+    {
+        for (int j = 0; j<column; ++j)
+        { matrix[i][j] *= num; }
+    }
+
+    return *this;
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column>& Matrix<type, row, column>::operator/=(int num)
+{ 
+    for (int i = 0; i<row; ++i)
+    {
+        for (int j = 0; j<column; ++j)
+        { matrix[i][j] /= num; }
+    }
+
+    return *this;
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column> Matrix<type, row, column>::operator/(int num)
 {
-    m = matr.size();
-    n = matr[0].size();
+    Matrix tmp {*this};
+    tmp /= num;
+    return tmp;
 }
 
-Matrix::Matrix(Matrix& second):
-m(second.m), 
-n(second.n),
-matr(second.matr)
-{}
-
-Matrix::~Matrix()
+template<class type, int row, int column>
+Matrix<type, row, column> Matrix<type, row, column>::operator*(Matrix another)
 {
-    while (!matr.empty())
-    { matr.pop_back(); }
-
-    m = 0;
-    n = 0;
+    Matrix copy {*this};
+    copy *= another;
+    return copy;
 }
 
-int Matrix::det()
+template<class type, int row, int column>
+Matrix<type, row, column> Matrix<type, row, column>::operator*(int num)
+{
+    Matrix copy {*this};
+    copy *= num;
+    return copy;
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column>& Matrix<type, row, column>::operator++()
+{ return *this += 1; }
+
+template<class type, int row, int column>
+Matrix<type, row, column> Matrix<type, row, column>::operator++(int)
+{
+    Matrix copy {*this};
+    *this += 1;
+    return copy;
+}
+
+template<class type, int row, int column>
+Matrix<type, row, column>& Matrix<type, row, column>::operator--()
+{ return *this -= 1; }
+
+template<class type, int row, int column>
+Matrix<type, row, column> Matrix<type, row, column>::operator--(int)
+{
+    Matrix copy {*this};
+    *this -= 1;
+    return copy;
+}
+
+template<class type, int row, int column>
+Matrix<type, row-1, column-1> Matrix<type, row, column>::minor(int in, int jn) const
+{
+    if (row == 1 | column == 1) throw UniversalError{"too small matrix"};
+
+    Matrix<type, row-1, column-1> tmp;
+    for (int i = 0; i<row; ++i)
+    {
+        for (int j = 0; j<column; ++j)
+        {
+            if ( i == in | j == jn ) continue;
+            tmp[i-(i>in)][j-(j>jn)] = matrix[i][j];
+        }
+    }
+
+    return tmp;
+}
+
+template<class type, int row, int column>
+int Matrix<type, row, column>::det() const
 {
     int i, j, d, k;
 
     j = 0; d = 0;
     k = 1; //(-1) в степени i
 
-    if (m < 1) return d;
-    if (m == 1) 
+    if (row < 1) return d;
+    if (row == 1) 
     {
-        d = matr[0][0];
+        d = matrix[0][0];
         return(d);
     }
-    if (m == 2) 
+    if (row == 2) 
     {
-        d = (matr[0][0] * matr[1][1]) - (matr[1][0] * matr[0][1]);
+        d = (matrix[0][0] * matrix[1][1]) - (matrix[1][0] * matrix[0][1]);
         return(d);
     }
-    if (m > 2) {
+    if (row > 2) {
         for (i = 0; i < m; i++) 
         {
-            d = d + k * matr[i][0] * (*this).minor(i, 0).det();
+            throw UniversalError{"мне лень прописывать пока что"};
+            // d = d + k * matrix[i][0] * (minor(i, 0).det());
+            // тут помогут концепты, если они заработают
             k = -k;
         }
     }
@@ -116,227 +299,81 @@ int Matrix::det()
     return(d);
 }
 
-Matrix Matrix::minor(int i, int j)
+template<class type, int row, int column>
+Matrix<type, row, column> Matrix<type, row, column>::transposition() const
 {
+    Matrix<type, column, row> tmp;
+    for (int i = 0; i<row; ++i)
+    {
+        for (int j = 0; j<column; ++j)
+        { tmp[j][i] = matrix[i][j]; }
+    }
 
-    Matrix copy {(*this)};
+    return tmp;
+}
+
+template<class type, int row, int column>
+void Matrix<type, row, column>::set(type value, int i, int j)
+{ matrix[i][j] = value; }
+
+template<class type, int row, int column>
+pair<type, type> Matrix<type, row, column>::get_size()
+{ return pair(m, n); }
+
+template<class type, int row, int column>
+type* Matrix<type, row, column>::operator[](int i)
+{ return matrix[i]; }
+
+template<class type, int row, int column>
+type* Matrix<type, row, column>::operator[](int i) const
+{ return matrix[i]; }
+
+template<class type, int row, int column>
+type& Matrix<type, row, column>::at(int i, int j)
+{ return matrix[i][j]; }
+
+template<class type, int row, int column>
+class Matrix<type, row, column>::UniversalError
+{
+    // абалдеть понты
+    // собственный класс для ошибок
     
-    copy.matr.erase(copy.matr.cbegin()+i);
-    copy.m--; copy.n--;
+    public:
+    UniversalError(): message(nullptr) {}
+    UniversalError(const char* message): message(message) {}
 
-    for(int a = 0; a<this->m; a++)
-    {
-        copy[a].erase(copy[a].cbegin()+j);
-    }
+    string getMessage() const { return message; }
 
-    return copy;
-}
+    protected:
+    string message;
+};
 
-Matrix Matrix::trans()
+template<class type, int row, int column>
+ostream& operator<<(ostream& out, Matrix<type, row, column> another)
 {
-    int fm = this->m;
-    int fn = this->n;
-    Matrix result {fm, fn};
+    for (int i = 0; i<row; ++i)
+    { 
+        for (int j = 0; j<column; ++j)
+        { out << another[i][j] << ' '; }
 
-    for (int i = 0; i<fm; i++)
-    {
-        for (int j = 0; j<fn; j++)
-        {
-            result[i][j] = (*this)[j][i];
-        }
-    }
-
-    return result;
-}
-
-Matrix Matrix::adj()
-{
-    if (m != n) return Matrix (0, 0, 0);
-
-    Matrix result {m, n};
-    int k = 1;
-
-    for (int i = 0; i<m; i++)
-    {
-        for (int j = 0; j<n; j++)
-        {
-            result[i][j] = k*(*this).minor(i, j).det();
-            k = -k;
-        }
-    }
-
-    return result;
-}
-
-Matrix Matrix::invert(int size, int rd)
-{
-    Matrix adj_key 
-    {
-        (
-            (
-                (
-                    (*this).adj()%size
-                )*rd
-            )%size
-        ).trans()
-    };
-
-    for (int i = 0; i<m; i++)
-    {
-        for (int j = 0; j<n; j++)
-        {
-            if (adj_key[i][j] < 0) 
-            adj_key[i][j] += size; 
-        }
-    }
-
-    return adj_key;
-}
-
-int Matrix::get_n()
-{ return n; }
-
-Matrix Matrix::operator+(Matrix& second)
-{
-    int fm = this->m, sm = second.m;
-    int fn = this->n, sn = second.n;
-
-    Matrix result {fm, fn};
-
-    if ((fm != sm) | (fn != sn)) 
-    return result;
-
-    for (int i = 0; i<fm; i++)
-    {
-        for (int j = 0; j<fn; j++)
-        {
-            result[i][j] = (*this)[i][j]+second[i][j];
-        }
-    }
-
-    return result; 
-}
-
-Matrix Matrix::operator-(Matrix& second)
-{
-    Matrix new_one {second*-1};
-    return *this + new_one;
-}
-
-Matrix Matrix::operator+(int c)
-{
-    Matrix second {m, n, c};
-    return *this + second;
-}
-
-Matrix Matrix::operator-(int c)
-{ return *this + (-c); }
-
-Matrix& Matrix::operator+=(Matrix& second)
-{
-    matr = (*this + second).matr;
-    return *this;
-}
-
-Matrix& Matrix::operator-=(Matrix& second)
-{
-    matr = (*this - second).matr;
-    return *this;
-}
-
-Matrix& Matrix::operator+=(int c)
-{
-    matr = (*this + c).matr;
-    return *this;
-}
-
-Matrix& Matrix::operator-=(int c)
-{
-    matr = (*this - c).matr;
-    return *this;
-}
-
-Matrix Matrix::operator*(Matrix& second)
-{
-    int fm = this->m, sm = second.m;
-    int fn = this->n, sn = second.n;
-
-    Matrix result {fm, sn, 0};
-
-    if (fn != sm) return result;
-
-    int a;
-    for (int i = 0; i<fm; i++)
-    {
-        for (int j = 0; j<sn; j++)
-        {
-            a = 0;
-            for(int z = 0; z<fn; z++)
-            {
-                a += (*this)[i][z] * second[z][j];
-            }
-            result[i][j] = a;
-        }
-    }
-
-    return result; 
-}
-
-Matrix Matrix::operator*(int c)
-{
-    int fm = this->m;
-    int fn = this->n;
-    Matrix result {fm, fn};
-
-    for (int i = 0; i<fm; i++)
-    {
-        for (int j = 0; j<fn; j++)
-        {
-            result[i][j] = (*this)[i][j]*c;
-        }
-    }
-
-    return result;
-}
-
-Matrix Matrix::operator%(int c)
-{
-    int fm = this->m;
-    int fn = this->n;
-    Matrix result {fm, fn};
-
-    for (int i = 0; i<fm; i++)
-    {
-        for (int j = 0; j<fn; j++)
-        {
-            result[i][j] = (*this)[i][j]%c;
-        }
-    }
-
-    return result;
-}
-
-vector<int>& Matrix::operator[](int c)
-{ return matr[c]; }
-
-
-ostream& operator<< (ostream& out, Matrix pups)
-{
-    vector<vector<int>> key = pups.matr;
-    for(int i = 0; i<key.size(); i++)
-    {
-        for (int j = 0; j<key[i].size(); j++)
-        {
-            out << key[i][j] << ' ';
-        }
         out << endl;
     }
 
     return out;
 }
 
-Matrix& operator++(Matrix& second)
-{ return second += 1; }
+template<class type, int row, int column>
+istream& operator>>(istream& in, Matrix<type, row, column>& another)
+{
+    type tmp;
+    for (int i = 0; i<row; ++i)
+    {
+        for (int j = 0; j<column; ++j)
+        {
+            in >> tmp;
+            another[i][j] = tmp; 
+        }
+    }
 
-Matrix& operator--(Matrix& second)
-{ return second -= 1; }
+    return in;
+}
